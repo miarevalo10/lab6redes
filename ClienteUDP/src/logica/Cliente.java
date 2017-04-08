2ffd9a4b46a61995ebd94fc156bea3379dc54621
María Arévalo <mi.arevalo10@uniandes.edu.co>
2017-04-08 18:04:35 -0500

    saca promedio y diferencia de tiempo

--- ClienteUDP/src/logica/Cliente.java
+++ ClienteUDP/src/logica/Cliente.java
@@ -3,6 +3,8 @@ package logica;
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
 import java.net.InetAddress;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
 import java.util.Date;
 
 public class Cliente {
@@ -12,16 +14,28 @@ public class Cliente {
 		int contador=1;
 		
         DatagramSocket sock = new DatagramSocket();
-        InetAddress host = InetAddress.getByName("localhost");
+        InetAddress host = InetAddress.getByName("54.187.39.221"); 
+        //--> Para probar con ec2 de aws
+//        InetAddress host = InetAddress.getByName("localhost");
+
+//        TimeZone tz = TimeZone.getTimeZone("GMT-05:00");
+		DateFormat df = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss:SS'Z'");
+//		df.setTimeZone(tz);
          
         while(contador<=cantidad)
         {
-            String s = "secuencia: "+contador+" timestamp" + new Date();
+        	
+			String timestamp = df.format(new Date());
+            String s = "secuencia: "+contador+" timestamp: " + timestamp ;
+            if(contador==1)
+            	s+= "total:  " + cantidad;
+            
+            System.out.println("Cliente envía: " + s);
             byte[] b = s.getBytes();
              
             DatagramPacket  dp = new DatagramPacket(b , b.length , host , puerto);
             sock.send(dp);
-            
+            System.out.println("despues de enviar cli");
             //now receive reply
             //buffer to receive incoming data
             byte[] buffer = new byte[65536];
@@ -30,7 +44,8 @@ public class Cliente {
              
             byte[] data = reply.getData();
             s = new String(data, 0, reply.getLength());
-            System.out.println(s);
+            System.out.println("Cliente recibe " + s);
+            contador++;
         }
         
         sock.close();
--- ServidorUDP/src/Servidor.java
+++ ServidorUDP/src/Servidor.java
@@ -1,22 +1,26 @@
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
 
 public class Servidor
 {
 	
+	private static ArrayList<InfoCliente> clientes;
 	public static void main(String args[])
 	{
 		int puerto = 8081;
 		DatagramSocket sock = null;
-		BufferedReader stdIn = new BufferedReader(new InputStreamReader(System.in));
+		clientes = new ArrayList<InfoCliente>();
 
 
-		try
+		try	
 		{
-			puerto = Integer.parseInt(stdIn.readLine());
+//			puerto = Integer.parseInt(args[0]); --> Desdocumentar para exportar a jar y pasar puerto por consola
 
 			//1. creating a server socket, parameter is local port number
 			sock = new DatagramSocket(puerto);
@@ -24,21 +28,61 @@ public class Servidor
 			//buffer to receive incoming data
 			byte[] buffer = new byte[65536];
 			DatagramPacket incoming = new DatagramPacket(buffer, buffer.length);
+			
+//			TimeZone tz = TimeZone.getTimeZone("GMT-05:00");
+			DateFormat df = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss:SS'Z'");
+//			df.setTimeZone(tz);
 
 			//2. Wait for an incoming data
 			System.out.println("Server socket created. Waiting for incoming data...");
+			
 
 			//communication loop
-			while(true)
+			while(true) 	
 			{
+
 				sock.receive(incoming);
 				byte[] data = incoming.getData();
+				
 				String s = new String(data, 0, incoming.getLength());
+				
+				//Atributos básicos de conexion
+				String ipCli = incoming.getAddress().getHostAddress();
+				int portCli = incoming.getPort();
 
 				//echo the details of incoming data - client ip : client port - client message
-				System.out.println(incoming.getAddress().getHostAddress() + " : " + incoming.getPort() + " - " + s);
+				System.out.println(ipCli + " : " + portCli + " - " + s);
+				
+				String timestampCli = s.split("timestamp: ")[1];
+				Date ts = df.parse(timestampCli);
+				long diferencia = (new Date().getTime() - ts.getTime());
+				
+				System.out.println("La diferencia es: " +diferencia+"ms");
+				String secuencia = s.split(" ")[1];
+				
+				InfoCliente cli=null;
+				
+				if(Integer.parseInt(secuencia)==1)
+				{
+					String total = s.split(" ")[5];
+					System.out.println("Numero de paquetes: " + total);
+					cli = new InfoCliente(ipCli,portCli,Integer.parseInt(total),clientes.size()+1);
+					clientes.add(cli);
+				}
+				else
+				{
+					//Si ya existe,	buscar el cliente en la lista para guardar la info
+					cli = darCliente(incoming.getAddress().getHostAddress(), incoming.getPort());
 
-				s = "OK : " + s;
+				}
+
+				s = secuencia + " : " + diferencia;
+				System.out.println("Servidor: " +  s + "ms");
+				
+			
+				cli.añadirSecuencia(secuencia, diferencia);
+				
+				
 				DatagramPacket dp = new DatagramPacket(s.getBytes() , s.getBytes().length , incoming.getAddress() , incoming.getPort());
 				sock.send(dp);
 			}
@@ -47,7 +91,27 @@ public class Servidor
 		catch(IOException e)
 		{
 			System.err.println("IOException " + e);
+		} 
+		catch (ParseException e) {
+			System.err.println("ParseExepction " + e);
+
 		}
+	}
+	
+	public static InfoCliente darCliente(String ip, int puerto)
+	{
+		InfoCliente cli = null;
+		for (int i = 0; i < clientes.size() && cli==null; i++) {
+			InfoCliente c = clientes.get(i);
+			if(c.getIp().equals(ip)&&c.getPuerto()==puerto)
+			{
+				cli=c;
+			}
+			
+		}
+		
+		
+		return cli;
 	}
 
 
